3dfa980792d87b31fc626915afb4bd3a10574bdc
TstrStrdl06 <145793790+TstrStrdl06@users.noreply.github.com>
2025-04-20 21:18:45 -0400

    Add files via upload

---
+++ Assets/Scripts/Slingshot.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slingshot : MonoBehaviour
+{
+	[Header("Set in Inspector")]
+	public GameObject prefabProjectile;
+	[Header("Set Dynamically")]
+	
+	public GameObject launchPoint;
+	public Vector3 launchPos;
+	public GameObject projectile;
+	public bool aimingMode;
+	
+	void Awake(){
+		Transform launchPointTrans = transform.Find("LaunchPoint");
+		launchPoint = launchPointTrans.gameObject;
+		launchPoint.SetActive(false);
+		launchPos = launchPointTrans.position;
+	}
+	
+	void OnMouseEnter(){
+		//print("Slingshot:OnMouseEnter()");
+		launchPoint.SetActive(true);
+	}
+	
+	void OnMouseExit(){
+		//print("Slingshot:OnMouseExit()");
+		launchPoint.SetActive(false);
+	}
+	
+	void OnMouseDown(){
+		aimingMode = true;
+		projectile = Instantiate(prefabProjectile) as GameObject;
+		// move it to the launch Pos
+		projectile.transform.position = launchPos;
+		projectile.GetComponent<Rigidbody>().isKinematic = true;
+	}
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
